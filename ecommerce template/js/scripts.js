8090cb52c3a0f6a79013b237a64101a3f0d811ba
Fran Ortega <franortega.wg@gmail.com>
2022-07-25 17:27:24 -0300

    template

--- ecommerce template/js/scripts.js
+++ ecommerce template/js/scripts.js
@@ -6,15 +6,16 @@ let carritoPrice = 0; //Precio total del carrito
 
 
 //Función constructora de un producto
-function Producto(id, name, price){
+function Producto(id, name, price, img){
     this.id = id;
     this.name = name;
     this.price = price;
+    this.img = img;
 }
 
 //Agregar un producto a la lista mediante la constructora
-function agregarProducto(id, name, price){
-    listaProductos.push(new Producto(id, name, price));
+function agregarProducto(id, name, price,img){
+    listaProductos.push(new Producto(id, name, price,img));
 } 
 
 //Remover un producto de la lista
@@ -31,6 +32,8 @@ function cart(id){
     myCart.innerHTML = carrito.length;
     carritoPrice += objeto.price;
     console.log(`Total actual del carrito: $${carritoPrice}`)
+    console.log(carrito)
+
 }
 
 //Remover producto del carrito
@@ -50,15 +53,15 @@ function filtrarPrecio(arr, priceFilter){
 }
 
 //Productos de mi tienda------------
-agregarProducto(1090, 'Chanclas',40);
-agregarProducto(1091, 'Gorra', 15);
-agregarProducto(1092, 'Medias', 5);
-agregarProducto(1093, 'Paraguas', 20);
-agregarProducto(1094, 'Zapatillas', 80);
-agregarProducto(1095, 'Camiseta', 60);
-agregarProducto(1096, 'Shorts', 30);
-agregarProducto(1097, 'Pelota', 40);
-agregarProducto(10, 'PruebaRemover', 90); 
+agregarProducto(1090, 'Chanclas',40,'img/chanclasDeBoca.jpg');
+agregarProducto(1091, 'Gorra', 15,'img/gorraDeBoca.jpg');
+agregarProducto(1092, 'Medias', 5, 'img/mediasDeBoca.jpeg');
+agregarProducto(1093, 'Paraguas', 20, 'img/paraguasDeBoca.jpeg');
+agregarProducto(1094, 'Zapatillas', 80, 'img/zapatillaDeBoca.jpg');
+agregarProducto(1095, 'Camiseta', 60, 'img/RemeraBoca.jfif');
+agregarProducto(1096, 'Shorts', 30, 'img/shortBoca.jfif');
+agregarProducto(1097, 'Pelota', 40, 'img/pelota.jfif');
+agregarProducto(10, 'PruebaRemover', 90, 'img/mediasDeBoca.jpeg'); 
 //----------------------------------
 
 //Prueba remover producto de la lista ✓
@@ -66,18 +69,19 @@ removerProductoList(10);
 // console.log(listaProductos);
 
 //Prueba remover producto del carrito ✓
-cart(1090); //Precio: 40
-cart(1093); //Precio: 20
-cart(1092)  //Precio: 5
-removerProductoCart(1090); // -40
-removerProductoCart(1093); // -20
-console.log(carrito);
-console.log(`Total actual del carrito: $${carritoPrice}`); //Precio esperado: 5
+// cart(1090); //Precio: 40
+// cart(1093); //Precio: 20
+// cart(1092)  //Precio: 5
+// removerProductoCart(1090); // -40
+// removerProductoCart(1092); // -5
+// removerProductoCart(1093); // -20
+// console.log(carrito);
+// console.log(`Total actual del carrito: $${carritoPrice}`); //Precio esperado: 5
 //----------------------------------
 
 //Prueba filtros dado precio ✓
 pruebaFiltro = filtrarPrecio(listaProductos, 20);
-console.log(pruebaFiltro); //Productos esperados: Medias, Gorra
+// console.log(pruebaFiltro); //Productos esperados: Medias, Gorra
 
 
 /*Pensé también en agregar un filtro por categorías del estilo:
@@ -92,10 +96,32 @@ Pero tenía que agregarle la categoría a todos los productos así que lo voy a 
 */
 //----------------------------------
 
+const productosEnTienda = document.querySelector('#products');
+
+const templateProduct = document.querySelector('#templateProduct').content;
+const fragment = document.createDocumentFragment();
+
+console.log(listaProductos)
+
+listaProductos.forEach(item => {
+    templateProduct.querySelector('h5').textContent = item.name;
+    templateProduct.querySelector('img').src = item.img;
+    templateProduct.querySelector('p').textContent = `$${item.price}.00`;
 
 
+    templateProduct.querySelector('a').setAttribute('onclick', `cart(${item.id})`)
 
 
+    const clone = templateProduct.cloneNode(true);
+    fragment.appendChild(clone);
+})
+productosEnTienda.appendChild(fragment);
+
+// const botones = document.querySelectorAll('#products a');
+
+// botones.forEach(el =>{
+//     el.addEventListener('click', () => cart())
+// })
 
 
 
