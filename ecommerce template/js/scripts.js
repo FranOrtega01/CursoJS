e8a8918dead6ab140182c8d52ca6bef7b58b7e77
Fran Ortega <franortega.wg@gmail.com>
2022-07-28 00:07:04 -0300

    entrega dom

--- ecommerce template/js/scripts.js
+++ ecommerce template/js/scripts.js
@@ -15,7 +15,7 @@ function Producto(id, name, price, img){
 
 //Agregar un producto a la lista mediante la constructora
 function agregarProducto(id, name, price,img){
-    listaProductos.push(new Producto(id, name, price,img));
+    listaProductos.push(new Producto(id, name, price, img));
 } 
 
 //Remover un producto de la lista
@@ -31,7 +31,7 @@ function cart(id){
     //Actualizar precio y numero del carrito
     myCart.innerHTML = carrito.length;
     carritoPrice += objeto.price;
-    console.log(`Total actual del carrito: $${carritoPrice}`)
+    console.log(`Total actual del carrito: $${carritoPrice}.00`)
     console.log(carrito)
 
 }
@@ -39,14 +39,21 @@ function cart(id){
 //Remover producto del carrito
 function removerProductoCart(id){
     let indexCart = carrito.findIndex((producto) => producto.id === id); //Busca el index del id indicado
+    if (carrito.length > 0 && indexCart !== -1){
+        carritoPrice -= carrito[indexCart].price; //Resta el precio del objeto con el index buscado
+        carrito.splice(indexCart,1); //Borra el elemento con la id indicada
+        myCart.innerHTML = carrito.length; //Actualiza el carrito
+        console.log(`Total actual del carrito: $${carritoPrice}.00`)
+        console.log(carrito);
+    }else if (carrito.length == 0){
+        console.log('El carrito esta vacio!')
+    }else{
+        console.log('El producto no esta en el carrito')
+    }
 
-    carritoPrice -= carrito[indexCart].price; //Resta el precio del objeto con el index buscado
-
-    if(indexCart !== -1) carrito.splice(indexCart,1); //Borra el elemento con la id indicada
-    myCart.innerHTML = carrito.length; //Actualiza el carrito
 }
 
-//Filtrar los productos por precio
+//Filtrar los productos por precio 
 function filtrarPrecio(arr, priceFilter){
     const filtro = arr.filter((el) => el.price < priceFilter);
     return filtro;
@@ -61,71 +68,53 @@ agregarProducto(1094, 'Zapatillas', 80, 'img/zapatillaDeBoca.jpg');
 agregarProducto(1095, 'Camiseta', 60, 'img/RemeraBoca.jfif');
 agregarProducto(1096, 'Shorts', 30, 'img/shortBoca.jfif');
 agregarProducto(1097, 'Pelota', 40, 'img/pelota.jfif');
-agregarProducto(10, 'PruebaRemover', 90, 'img/mediasDeBoca.jpeg'); 
-//----------------------------------
-
-//Prueba remover producto de la lista ✓
-removerProductoList(10); 
-// console.log(listaProductos);
-
-//Prueba remover producto del carrito ✓
-// cart(1090); //Precio: 40
-// cart(1093); //Precio: 20
-// cart(1092)  //Precio: 5
-// removerProductoCart(1090); // -40
-// removerProductoCart(1092); // -5
-// removerProductoCart(1093); // -20
-// console.log(carrito);
-// console.log(`Total actual del carrito: $${carritoPrice}`); //Precio esperado: 5
 //----------------------------------
 
 //Prueba filtros dado precio ✓
-pruebaFiltro = filtrarPrecio(listaProductos, 20);
+// pruebaFiltro = filtrarPrecio(listaProductos, 20);
 // console.log(pruebaFiltro); //Productos esperados: Medias, Gorra
 
-
-/*Pensé también en agregar un filtro por categorías del estilo:
-
+/* Idea
 function filtrarPrecio(arr, categoriaFilter){
     const filtro = arr.filter((el) => el.name.includes('categoriaFilter'));
     return filtro;
 }
 pruebaCategoria = filtrarPrecio(listaProductos, 'shorts');
-
-Pero tenía que agregarle la categoría a todos los productos así que lo voy a dejar para la prox entrega con DOM!
 */
 //----------------------------------
 
+//Template de productos ✓
 const productosEnTienda = document.querySelector('#products');
-
 const templateProduct = document.querySelector('#templateProduct').content;
 const fragment = document.createDocumentFragment();
-
-console.log(listaProductos)
 
 listaProductos.forEach(item => {
     templateProduct.querySelector('h5').textContent = item.name;
     templateProduct.querySelector('img').src = item.img;
     templateProduct.querySelector('p').textContent = `$${item.price}.00`;
-
-
-    templateProduct.querySelector('a').setAttribute('onclick', `cart(${item.id})`)
-
-
+    templateProduct.querySelector('.add').setAttribute('onclick', `cart(${item.id})`);
+    templateProduct.querySelector('.remove').setAttribute('onclick', `removerProductoCart(${item.id})`);
     const clone = templateProduct.cloneNode(true);
     fragment.appendChild(clone);
-})
+});
 productosEnTienda.appendChild(fragment);
-
-// const botones = document.querySelectorAll('#products a');
-
-// botones.forEach(el =>{
-//     el.addEventListener('click', () => cart())
-// })
+//----------------------------------
 
 
+//Actualizar numero con carrito  x
+    //Pendiente, detectar cambio en el array carrito e imprimir su longitud en myCart.innerHTML
+//----------------------------------
 
 
+//Vaciar el carrito ✓
+const btnVaciarCarrito = document.getElementById('borrarCarrito');
+btnVaciarCarrito.addEventListener('click', () => {
+    carrito.splice(0,carrito.length);
+    myCart.innerHTML = carrito.length;
+    carritoPrice = 0;
+    console.log('Se vació el carrito!')
+});
+//----------------------------------
 
 
 
